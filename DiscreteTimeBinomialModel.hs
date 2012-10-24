0391e9a780dc7995994398ee8d4d06e0cc472534
Ashwin Siripurapu <ashwins1@stanford.edu>
2012-10-23 19:21:52 -0700

    More TODOs...

--- DiscreteTimeBinomialModel.hs
+++ DiscreteTimeBinomialModel.hs
@@ -9,6 +9,8 @@ data Input = Input { strikePrice       :: Double
                    , interestRate      :: Double
                    }
 
+-- TODO: compute the price of the option at any time prior to maturity
+-- (perhaps have an Output type that configures this, and other things?)
 priceOption :: Model -> Input -> Double
 priceOption m i = priceOption' m i 0 0
   where priceOption' m i period ups
